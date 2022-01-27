6b40a8da660a1b64e6e5f6ce7c7f43b9246d6fe0
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-01-27 14:09:47 +0200

    Add black, flake, to null-ls

--- nvim/.config/nvim/lua/config/null-ls.lua
+++ nvim/.config/nvim/lua/config/null-ls.lua
@@ -1,10 +1,14 @@
 local null_ls = require("null-ls")
 
-null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettier.with({
-			only_local = "node_modules/.bin",
-		}),
-	},
-})
+local sources = {
+	null_ls.builtins.formatting.black,
+	null_ls.builtins.diagnostics.mypy,
+	null_ls.builtins.diagnostics.flake8,
+
+	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.formatting.prettier.with({
+		only_local = "node_modules/.bin",
+	}),
+}
+
+null_ls.setup({ sources = sources })
