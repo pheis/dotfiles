205a0e408e1b6962d231c79815affa4f4009bd93
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-05-21 15:21:00 +0300

    Add abbrevs to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,14 @@ if status is-interactive
 
 end
 
+abbr -a -U -- f fuck
+abbr -a -U -- g git
+abbr -a -U -- gs 'git status'
+abbr -a -U -- ls exa
+abbr -a -U -- p python3
+abbr -a -U -- t tmux
+abbr -a -U -- v nvim
+
 direnv hook fish | source
 zoxide init fish | source
 thefuck --alias | source
