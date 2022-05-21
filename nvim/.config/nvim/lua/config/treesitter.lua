07d26f3f7a2245f59a39b9275ba8bfb45c25d105
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-05-21 15:19:41 +0300

    Add fish to treesitter

--- nvim/.config/nvim/lua/config/treesitter.lua
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -1,6 +1,20 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "tsx", "typescript", "python", "json", "http" },
+  -- ensure_installed = "all",
+  ensure_installed = {
+    "c",
+    "lua",
+    "rust",
+    "tsx",
+    "typescript",
+    "javascript",
+    "python",
+    "json",
+    "http",
+    "fish",
+    "bash",
+    "yaml"
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
