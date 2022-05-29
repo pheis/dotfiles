a3500de64f19bb63e10aa3cb7460192126df932a
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-05-29 14:01:22 +0300

    add comment about gitsigns null-ls

--- nvim/.config/nvim/lua/config/null-ls.lua
+++ nvim/.config/nvim/lua/config/null-ls.lua
@@ -1,14 +1,17 @@
 local null_ls = require("null-ls")
 
 local sources = {
-	null_ls.builtins.formatting.black,
-	null_ls.builtins.diagnostics.mypy,
-	null_ls.builtins.diagnostics.flake8,
+  null_ls.builtins.formatting.black,
+  null_ls.builtins.diagnostics.mypy,
+  null_ls.builtins.diagnostics.flake8,
 
-	-- null_ls.builtins.formatting.stylua,
-	null_ls.builtins.formatting.prettier.with({
-		only_local = "node_modules/.bin",
-	}),
+  -- too irritating to use, git blame does not need to be here
+  -- null_ls.builtins.code_actions.gitsigns,
+
+  -- null_ls.builtins.formatting.stylua,
+  null_ls.builtins.formatting.prettier.with({
+    only_local = "node_modules/.bin",
+  }),
 }
 
 null_ls.setup({ sources = sources })
