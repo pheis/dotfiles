86da37b6e67e78605fccbe074e3ddc90d38ee902
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-06-01 21:41:10 +0300

    require keys, map leader-q to reloading

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,15 @@ require("plugins")
 require("toggle")
 
 require("p.ts-run")
+require("p.keys")
 
 vim.cmd("colo gruvbox")
 
 vim.opt.laststatus = 3
+
+
+vim.keymap.set('n', '<leader>q', function()
+  require('plenary.reload').reload_module('plugins')
+  require('plenary.reload').reload_module('config')
+  require('plenary.reload').reload_module("p.ts-run")
+end)
