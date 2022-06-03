807dd3ea7727d046bcbb03c15f491b2b8c61a068
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-06-03 16:38:59 +0300

    more telescope keys

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,10 +1,17 @@
 require("telescope").setup({})
 
+-- TODO <leader>fd --> list files different than base branch (develop/main/master)
+
 vim.cmd([[
 nnoremap <leader>/ <cmd>Telescope live_grep<cr>
 nnoremap <leader>b <cmd>Telescope buffers<cr>
 nnoremap <leader>* <cmd>Telescope lsp_references<cr>
-nnoremap <leader>f <cmd>Telescope find_files<cr>
+
+nnoremap <leader>fa <cmd>Telescope find_files<cr>
+nnoremap <leader>fg <cmd>Telescope git_status<cr>
+nnoremap <leader>fo <cmd>Telescope oldfiles<cr>
+nnoremap <leader>fq <cmd>Telescope quickfix<cr>
+
 nnoremap <leader>s <cmd>Telescope lsp_document_symbols<cr>
 nnoremap <leader>S <cmd>Telescope lsp_workspace_symbols<cr>
 ]])
