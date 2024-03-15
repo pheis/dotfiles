34935de3afa5a8d0e6faa3d712f1065042ccfdd8
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2024-03-15 14:12:27 +0200

    Neotree setup

--- nvim-new/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim-new/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,32 +1,40 @@
--- TODO: update HL on side bar automatically based on current buffer
-
 local setup = function()
   vim.keymap.set('n', '<C-t>', ':Neotree toggle<cr>', { desc = 'toggle neotree' })
+  vim.keymap.set('n', '-', function()
+      local reveal_file = vim.fn.expand('%:p')
+      if (reveal_file == '') then
+        reveal_file = vim.fn.getcwd()
+      else
+        local f = io.open(reveal_file, "r")
+        if (f) then
+          f.close(f)
+        else
+          reveal_file = vim.fn.getcwd()
+        end
+      end
+      require('neo-tree.command').execute({
+        action = "focus",          -- OPTIONAL, this is the default value
+        source = "filesystem",     -- OPTIONAL, this is the default value
+        position = "current",         -- OPTIONAL, this is the default value
+        reveal_file = reveal_file, -- path to file or folder to reveal
+        reveal_force_cwd = true,   -- change cwd without asking if needed
+      })
+    end,
+    { desc = "Open neo-tree at current file or working directory" }
+  ); -- nnoremap <leader>s :Neotree float git_status<cr>
+
+  require('neo-tree').setup({
+    filesystem = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true
+      },
+      use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+    },
+  })
 end
 
-    -- nnoremap <leader>b :Neotree toggle show buffers right<cr>
-vim.keymap.set('n', '-', function()
-    local reveal_file = vim.fn.expand('%:p')
-    if (reveal_file == '') then
-      reveal_file = vim.fn.getcwd()
-    else
-      local f = io.open(reveal_file, "r")
-      if (f) then
-        f.close(f)
-      else
-        reveal_file = vim.fn.getcwd()
-      end
-    end
-    require('neo-tree.command').execute({
-      action = "focus",          -- OPTIONAL, this is the default value
-      source = "filesystem",     -- OPTIONAL, this is the default value
-      position = "left",         -- OPTIONAL, this is the default value
-      reveal_file = reveal_file, -- path to file or folder to reveal
-      reveal_force_cwd = true,   -- change cwd without asking if needed
-    })
-    end,
-    { desc = "Open neo-tree at current file or working directory"}
-  );   -- nnoremap <leader>s :Neotree float git_status<cr>
+-- nnoremap <leader>b :Neotree toggle show buffers right<cr>
 
 return {
   "nvim-neo-tree/neo-tree.nvim",
