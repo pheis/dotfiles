098c881881871cce5b39ed3ac4fc364dc94326da
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2024-03-28 11:46:54 +0200

    drop lint

--- home/shared/nvim/.config/nvim/lua/plugins/lint.lua
+++ home/shared/nvim/.config/nvim/lua/plugins/lint.lua
@@ -1,16 +1,21 @@
 local config = function()
-  local lint = require("lint")
+  -- local lint = require("lint")
+  --
+  -- lint.linters_by_ft = {
+  --   typescript = { "eslint" },
+  --   tsx = { "eslint" },
+  -- }
 
-  lint.linters_by_ft = {
-    typescript = { "eslint" },
-    tsx = { "eslint" },
-  }
-
-  vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    callback = function()
-      lint.try_lint()
-    end,
-  })
+  -- InsertLeave
+  -- TextChanged
+  -- todo: only call lint if lint is not already running?
+  -- if it is already running, postpone the next call to only call it after last
+  -- lint has finished
+  -- vim.api.nvim_create_autocmd({ "TextChanged" }, {
+  --   callback = function()
+  --     lint.try_lint()
+  --   end,
+  -- })
 end
 
 return {
