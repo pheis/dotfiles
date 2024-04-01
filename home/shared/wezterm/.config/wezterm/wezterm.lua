d4e8cbe89f5a79d9450351a9eb24906b2e98b405
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2024-04-01 20:37:34 +0300

    fix wezterm cursor color

--- home/shared/wezterm/.config/wezterm/wezterm.lua
+++ home/shared/wezterm/.config/wezterm/wezterm.lua
@@ -43,13 +43,10 @@ config.colors = {
 
   -- Overrides the cell background color when the current cell is occupied by the
   -- cursor and the cursor style is set to Block
-  cursor_bg = '#52ad70',
-  -- Overrides the text color when the current cell is occupied by the cursor
-  cursor_fg = 'black',
-  -- Specifies the border color of the cursor when the cursor style is set to Block,
-  -- or the color of the vertical or horizontal bar when the cursor style is set to
-  -- Bar or Underline.
-  cursor_border = '#52ad70',
+  cursor_bg = rgb(0xd8, 0xd8, 0xd8),
+  cursor_border = rgb(0xd8, 0xd8, 0xd8),
+  cursor_fg = rgb(0x18, 0x18, 0x18),
+
 
   -- the foreground color of selected text
   selection_fg = 'black',
