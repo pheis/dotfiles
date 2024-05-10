c4029507dc2368f155f5947121d0c50deb34b1db
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2024-05-10 14:37:38 +0300

    Add colmena

--- nix/flake.nix
+++ nix/flake.nix
@@ -51,5 +51,14 @@
           ];
         };
       };
+      colmena = {
+        meta = {
+          nixpkgs = import nixpkgs { system = "x86_64-linux"; };
+          specialArgs = {
+
+          };
+        };
+        koti = import ./hosts/koti/default.nix;
+      };
     };
 }
