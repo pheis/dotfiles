cedd94028fcc7bffd430a20d156b17a1c2f089a5
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2024-06-14 13:45:29 +0300

    Disable neotree -

--- home/shared/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ home/shared/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,26 +6,30 @@ local setup = function()
     { desc = "toggle neotree" }
   )
 
-  vim.keymap.set("n", "-", function()
-    local reveal_file = vim.fn.expand("%:p")
-    if reveal_file == "" then
-      reveal_file = vim.fn.getcwd()
-    else
-      local f = io.open(reveal_file, "r")
-      if f then
-        f.close(f)
-      else
-        reveal_file = vim.fn.getcwd()
-      end
-    end
-    require("neo-tree.command").execute({
-      action = "focus",
-      source = "filesystem",
-      position = "current",
-      reveal_file = reveal_file,
-      reveal_force_cwd = true,
-    })
-  end, { desc = "Open neo-tree at current file or working directory" }) -- nnoremap <leader>s :Neotree float git_status<cr>
+  -- vim.keymap.set("n", "-", function()
+  --   local reveal_file = vim.fn.expand("%:p")
+  --   if reveal_file == "" then
+  --     reveal_file = vim.fn.getcwd()
+  --   else
+  --     local f = io.open(reveal_file, "r")
+  --     if f then
+  --       f.close(f)
+  --     else
+  --       reveal_file = vim.fn.getcwd()
+  --     end
+  --   end
+  --   require("neo-tree.command").execute({
+  --     action = "focus",
+  --     source = "filesystem",
+  --     position = "current",
+  --     reveal_file = reveal_file,
+  --     reveal_force_cwd = true,
+  --   })
+  -- end, { desc = "Open neo-tree at current file or working directory" })
+
+  -- nnoremap <leader>s :Neotree float git_status<cr>
+
+
 
   require("neo-tree").setup({
     filesystem = {
