79f24cb0d1bdd8eeb5e8386d3b3742e8ca1f83e7
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2025-03-03 12:38:19 +0200

    fix format

--- home/shared/nvim/.config/nvim/lua/plugins/lsp.lua
+++ home/shared/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -61,7 +61,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     -- Create a command `:Format` local to the LSP buffer
     vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-      vim.lsp.buf.format()
+      vim.lsp.buf.format({ filter = function(cli) return cli.name ~= "vtsls" end })
     end, { desc = "Format current buffer with LSP" })
   end,
 })
