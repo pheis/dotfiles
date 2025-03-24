e7239b9a42aaac56a4704c39916d62ddb27c7fc7
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2025-03-24 10:07:09 +0200

    nix upd

--- nix/darwin/nix-core.nix
+++ nix/darwin/nix-core.nix
@@ -13,6 +13,5 @@
   };
 
   # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 }
--- nix/darwin/system.nix
+++ nix/darwin/system.nix
@@ -10,6 +10,7 @@
 ###################################################################################
 {
 
+  # ids.uids.nixbld = 30000;
   system = {
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
     activationScripts.postUserActivation.text = ''
@@ -31,9 +32,6 @@
       # ......
     };
   };
-
-  # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   # this is required if you want to use darwin's default shell - zsh
