26535c58374eec4529fafda87da7a6b1e16c9939
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2026-02-03 15:25:38 +0200

    update

--- nix/deploy.sh
+++ nix/deploy.sh
@@ -3,4 +3,4 @@
 current_host=$(uname -n)
 
 nix build .#darwinConfigurations."$current_host".system
-./result/sw/bin/darwin-rebuild switch --flake .#"$current_host"
+sudo ./result/sw/bin/darwin-rebuild switch --flake .#"$current_host"
